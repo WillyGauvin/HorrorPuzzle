ec2063c343d9989905883e6725f5722377469938
Will <williamgauvin6098@gmail.com>
2025-06-23 15:23:59 -0400

    Banana works and monkey reacts to it. Creating inventory systmefor player to select between different items

---
+++ Source/Horror/Item.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "Item.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORROR_API AItem : public AInteractable
+{
+	GENERATED_BODY()
+	
+public:
+
+	AItem();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void HoldAction();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ReleaseAction();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CancelAction();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int MaxInInventory;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* InventoryIcon;
+};
